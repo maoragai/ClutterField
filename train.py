53b8bf926b7060b6d3c8a8d7234f4b6d6e9495eb
maorag <maor.agai@axon-pulse.com>
2026-05-28 01:14:15 +0300

    - multiresolution hash encoding checkboard

--- train.py
+++ train.py
@@ -14,24 +14,41 @@ if __name__ == "__main__":
 
     radius = 0.5
     criterion = torch.nn.MSELoss()
-    coarse_grid_res=8
-    mid_grid_res = coarse_grid_res * 2
-    fine_grid_res = coarse_grid_res * 4
-    ultra_grid_res = coarse_grid_res * 8
+    coarse_grid_res=32
+    mid_grid_res = coarse_grid_res * 2 
+    fine_grid_res = coarse_grid_res  * 4
+    ultra_grid_res = coarse_grid_res    * 8
     feature_dim=4
     
-    grid_shape = (mid_grid_res, mid_grid_res, feature_dim)
+    # grid_shape = (mid_grid_res, mid_grid_res, feature_dim)
+    def spatial_hash(coords:torch.Tensor, 
+                     table_size:torch.Tensor):
+        x = coords[:, 0].long()
+        y = coords[:, 1].long()
+
+        # Simple hash: (x * prime1 + y * prime2) % table_size
+        prime1 = 73856093
+        prime2 = 19349663
+        table_size = table_size 
+
+        indices = (x * prime1 + y * prime2) % table_size
+
+        return indices
+    
     class GridEncoder(nn.Module):
         def __init__(self, grid_shape):
             super().__init__()
+            self.resolution = grid_shape[0]
+            self.table_size = grid_shape[1]
+            self.feature_dim = grid_shape[2]
             self.grid_shape = grid_shape
-            self.grid =nn.Parameter(torch.randn(*grid_shape)*0.01)
+            self.hash_table = nn.Parameter(torch.randn(*grid_shape[-2:]) * 0.01)
+        
         def forward(self, coords):
 
-            # Map from [-1,1] → [0,res-1]
+            # [-1, 1] -> [0, grid_size -1]
             coords = (coords + 1) / 2
-            coords = coords * (self.grid_shape[0] - 1)
-
+            coords = coords * (self.resolution - 1)
             x = coords[:, 0]
             y = coords[:, 1]
 
@@ -43,18 +60,21 @@ if __name__ == "__main__":
             y1 = y0 + 1
 
             # Clamp bounds
-            x0 = torch.clamp(x0, 0, self.grid_shape[0] - 1)
-            x1 = torch.clamp(x1, 0, self.grid_shape[0] - 1)
+            x0 = torch.clamp(x0, 0, self.resolution - 1)
+            x1 = torch.clamp(x1, 0, self.resolution - 1)
 
-            y0 = torch.clamp(y0, 0, self.grid_shape[1] - 1)
-            y1 = torch.clamp(y1, 0, self.grid_shape[1] - 1)
-
+            y0 = torch.clamp(y0, 0, self.resolution - 1)
+            y1 = torch.clamp(y1, 0, self.resolution - 1)
             # Retrieve corner features
-            f00 = self.grid[x0, y0]
-            f10 = self.grid[x1, y0]
-            f01 = self.grid[x0, y1]
-            f11 = self.grid[x1, y1]
-
+            id00 = spatial_hash(torch.stack([x0.float(), y0.float()], dim=-1), self.table_size)  
+            id10 = spatial_hash(torch.stack([x1.float(), y0.float()], dim=-1), self.table_size)
+            id01 = spatial_hash(torch.stack([x0.float(), y1.float()], dim=-1), self.table_size)
+            id11 = spatial_hash(torch.stack([x1.float(), y1.float()], dim=-1), self.table_size)
+            
+            f00 = self.hash_table[id00]
+            f10 = self.hash_table[id10]
+            f01 = self.hash_table[id01]
+            f11 = self.hash_table[id11]
             # Fractional offsets
             wx = (x - x0.float()).unsqueeze(-1)
             wy = (y - y0.float()).unsqueeze(-1)
@@ -69,21 +89,22 @@ if __name__ == "__main__":
             return features
     
     
-    class MultiResolutionGridEncoder(nn.Module):
+    class MultiResolutionHashGridEncoder(nn.Module):
 
             def __init__(self,
-                        resolutions,
+                        resolutions:list[int],
+                        table_size:int,
                         feature_dim):
 
                 super().__init__()
 
                 self.encoders = nn.ModuleList()
 
-                for res in resolutions:
+                for resolution in resolutions:
 
                     grid_shape = (
-                        res,
-                        res,
+                        resolution,
+                        table_size,
                         feature_dim
                     )
 
@@ -102,8 +123,9 @@ if __name__ == "__main__":
                     features.append(feat)
 
                 return torch.cat(features, dim=-1)
-    gridencoder = MultiResolutionGridEncoder(
+    gridencoder = MultiResolutionHashGridEncoder(
         resolutions=[coarse_grid_res, mid_grid_res, fine_grid_res, ultra_grid_res],
+        table_size=2048,
         feature_dim=feature_dim
         )
 
@@ -115,10 +137,14 @@ if __name__ == "__main__":
     for epoch in tqdm(range(1000)):
         inputs = torch.rand(1024, 2) * 2 - 1  # Random points in [-1, 1]
         # Circle occupancy labels
+        freq = 10.0
+
         targets = (
-            torch.sum(inputs ** 2, dim=1, keepdim=True)
-            < radius ** 2
-        ).float()
+            (
+                torch.sin(freq * inputs[:,0]) *
+                torch.sin(freq * inputs[:,1])
+            ) > 0
+        ).float().unsqueeze(-1)
 
         # Forward pass
         outputs = model(gridencoder(inputs))
